4a3eebb372f74d75ad66585e0462707fe6071efc
CK1-CK <65177756+CK1-CK@users.noreply.github.com>
2020-11-04 10:22:46 +0100

    Überarbeitung. UTC convert in Lokale Zeitzone

--- hue_motion.py
+++ hue_motion.py
@@ -7,20 +7,42 @@ import requests
 import json
 import hue_getBridgeHttpAddress
 import hue_userKey
+import re
+from datetime import datetime,tzinfo,timezone
 
 
 motion_sensor_id=15
 
 hue_user=hue_userKey.GetHueUserKey()  #Hue Benutzerschlüssel
-
 http_address=hue_getBridgeHttpAddress.GetBridgeHttpAddress()
 
 sensor = http_address+'api/'+hue_user+'/sensors/'+str(motion_sensor_id)
 
 try:
     r = requests.get(sensor)
-    print(str(r.json()["state"]["presence"])) #Aktuell Bewegung?
-    print(str(r.json()["state"]["lastupdated"])) #Letzte Bewegung auslesen
-    
 except:
     print("Error-Verbindungsaufbau")
+    exit(-1)
+
+try:
+    print(str(r.json()["state"]["presence"])) #Aktuell Bewegung?
+    #print(str(r.json()["state"]["lastupdated"])) #Letzte Bewegung auslesen UTC
+
+    m = re.search('(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)T(\\d\\d):(\\d\\d):(\\d\\d)', str(r.json()["state"]["lastupdated"]))
+
+    #print(m.group(1)) #Jahr
+    #print(m.group(2)) #Monat
+    #print(m.group(3)) #Tag
+    #print(m.group(4)) #Stunde
+    #print(m.group(5)) #Minute
+    #print(m.group(6)) #Sekunde
+
+    last_motion_utc=datetime(int(m.group(1)),int(m.group(2)),int(m.group(3)),int(m.group(4)),int(m.group(5)),int(m.group(6)))
+    last_motion_localTime=last_motion_utc.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    
+    #print(last_motion_utc) #Letzte Bewegung UTC
+    print(last_motion_localTime) #Letzte Bewegung Local Time
+
+except:
+    print("Error")
+    exit(-1)
