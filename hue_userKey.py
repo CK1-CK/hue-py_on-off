0a84c0bbc3156ebbcf062e16a1ae55d518b46dd6
CK1-CK <65177756+CK1-CK@users.noreply.github.com>
2020-11-05 21:21:40 +0100

    Add

---
+++ hue_userKey.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+UserKey='xxxxxxxxxxxxxxxxxxxxxxxxxxx' #Hue Benutzerschlüssel
+
+def GetHueUserKey():
+    return UserKey
+
+
+if __name__ == '__main__':
+    GetHueUserKey()
